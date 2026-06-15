56626d7d67dab3359fb1b5da58c896f7c8a3a1d8
geppp41-school <1118807@apps.nsd.org>
2026-06-15 20:39:23 +0000

    started creating a system to adjust gui scales and locations

--- start_screen/Gui.cs
+++ start_screen/Gui.cs
@@ -1,19 +1,51 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 [Tool]
 public partial class Gui : Control
 {
 	private const int WindowHeight = 648;
 	private const int WindowWidth = 1152;
+	private int CurrentWindowHeight = 648;
+	private int CurrentWindowWidth = 1152;
+	[Export]public Control[] ControlItems = new Control[] {};
+	[Export]public Node2D[] Node2DItems = new Node2D[] {};
+	Dictionary<Object, Vector2> ItemScales = new Dictionary<object, Vector2>();
+	Dictionary<Object, Vector2> ItemPositions = new Dictionary<object, Vector2>();
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		
+		this.SetDefaultScales();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void SetDefaultScales()
+	{
+		foreach(var item in this.ControlItems)
+		{
+			this.ItemScales.Add(item, item.Scale);
+		}
+		foreach(var item in this.Node2DItems)
+		{
+			this.ItemScales.Add(item, item.Scale);
+		}
+	}
+
+	public void SetDefaultPositions()
+	{
+		foreach(var item in this.ControlItems)
+		{
+			this.ItemPositions.Add(item, item.Position);
+		}
+		foreach(var item in this.Node2DItems)
+		{
+			this.ItemPositions.Add(item, item.Position);
+		}
 	}
 }
