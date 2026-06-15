142e78c3bb59ac89222d718c74b592065aba1d0d
geppp41-school <1118807@apps.nsd.org>
2026-06-15 20:09:09 +0000

    getting started on menu sizing functionality

--- start_screen/Gui.cs
+++ start_screen/Gui.cs
@@ -4,9 +4,12 @@ using System;
 [Tool]
 public partial class Gui : Control
 {
+	private const int WindowHeight = 648;
+	private const int WindowWidth = 1152;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
